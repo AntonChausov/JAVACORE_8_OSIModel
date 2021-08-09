6ba314400f02b24da3cc35c946c909d6bed994d9
AntonChausov <antonchausov@gmail.com>
2021-08-09 21:21:58 +0300

    Feature

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,22 +2,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) throws UnknownHostException {
-        String host= "localhost";
+        String host= "netology.homework";
         int port= 8090;
+        InetAddress inetAddress = InetAddress.getByName(host);
+        System.out.println(host + ", ip address: " + inetAddress.getHostAddress());
+
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("Client");
-            //out.println("GET / HTTP/1.1\n" +"Host: " + host + "\n\n\n");
-            String resp = in.readLine();
-            System.out.println(resp);
+            out.println("Hi!");
+            String resp;
+            while (true) {
+                String answer = "empty";
+                resp = in.readLine();
+                System.out.println("> " + resp);
+                if (resp.contains("What is your name?")) {
+                    answer = "Pasha";
+                } else if (resp.contains("Where are you from?")){
+                    answer = "Russia";
+                }else if (resp.contains("Are you child? (yes/no)")){
+                    answer = "yes";
+                } else if (resp.contains("/end")){
+                    break;
+                }
+                System.out.println("< " + answer);
+                out.println(answer);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -14,8 +14,18 @@ public class Server {
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             System.out.println("New connection accepted");
+            String string = in.readLine();
+            out.println("Hi. What is your name?");
             final String name = in.readLine();
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            out.println(String.format("Hi %s, Where are you from?", name));
+            final String from = in.readLine();
+            out.println(String.format("Hi %s from %s, Are you child? (yes/no)", name, from));
+            final boolean itsChild = in.readLine().equals("yes");
+            if (itsChild){
+                out.println(String.format("Welcome to the kids area, %s from %s! Let's play! /end", name, from));
+            } else {
+                out.println(String.format("Welcome to the adult zone, %s from %s! Have a good rest, or a good working day! /end", name, from));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
